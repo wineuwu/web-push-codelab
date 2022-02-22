776f738d6c587e7974541226cf1ca9a70497cf47
winnie <asgh03226@gmail.com>
2022-02-22 18:01:54 +0800

    1.待修正訂閱功能 無法接收訊息 2.暫時將原作者設定的 eslint關起來

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,27 +1,31 @@
 /*
-*
-*  Push Notifications codelab
-*  Copyright 2015 Google Inc. All rights reserved.
-*
-*  Licensed under the Apache License, Version 2.0 (the "License");
-*  you may not use this file except in compliance with the License.
-*  You may obtain a copy of the License at
-*
-*      https://www.apache.org/licenses/LICENSE-2.0
-*
-*  Unless required by applicable law or agreed to in writing, software
-*  distributed under the License is distributed on an "AS IS" BASIS,
-*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-*  See the License for the specific language governing permissions and
-*  limitations under the License
-*
-*/
+ *
+ *  Push Notifications codelab
+ *  Copyright 2015 Google Inc. All rights reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License
+ *
+ */
 
 /* eslint-env browser, es6 */
+/*
+https://developers.google.com/web/fundamentals/codelabs/push-notifications#%E7%8D%B2%E5%8F%96%E6%87%89%E7%94%A8%E6%9C%8D%E5%8B%99%E5%99%A8%E5%AF%86%E9%91%B0
+鑰匙
+https://web-push-codelab.glitch.me/
+*/
 
 'use strict';
-
-const applicationServerPublicKey = '<Your Public Key>';
+const applicationServerPublicKey = 'your key';
 
 const pushButton = document.querySelector('.js-push-btn');
 
